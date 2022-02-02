c17abd89eff62c430dcc195a85f6e1c934af9914
Steve Marks <stevenmarks75@gmail.com>
2022-02-02 11:15:48 +0000

    form test

---
+++ form-to-email.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'yourname@yourwebsite.com';
+	$email_subject = "New Website Enquiry submission";
+	$email_body = $message;
+
+    $to = 'stevenmarks75@gmail.com';
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$message,$headers)
+
+
+}
+
+?>
